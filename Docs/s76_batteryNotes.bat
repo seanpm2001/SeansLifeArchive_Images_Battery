70da1258e706bb1dab09592f82c2022ac9d28844
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-26 22:50:45 -0700

    2 note files
    2 note file for this directory, in bat and dev formats.

---
+++ Docs/s76_batteryNotes.bat
@@ -0,0 +1,5 @@
+Ubuntu 22.04 SYS76 battery notes
+
+Battery icon is green when charging, Ubuntu 20.04 only had a white battery when charging
+Charging gets stuck at 98% for over 20 minutes, instead of 99%, before going to 100%
+Ubuntu 22.04 has 3 battery modes: battery life, balanced, and high performance
